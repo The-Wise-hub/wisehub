d7e28bfe1c80d5da8c4ed4e7eb643885a1f6670b
Amitabh Anand <amitabh.anandcl@gmail.com>
2023-11-29 21:23:06 +0530

    fix(apps): set for github actions

--- apps/backend/apis/src/app/app.service.ts
+++ apps/backend/apis/src/app/app.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { environment } from '../environments/environment';
 
 @Injectable()
 export class AppService {
-  getData(): { message: string } {
-    return { message: 'Hello API V1' };
+  getData(): { message: string; isProd: boolean } {
+    return { message: 'Hello API V1', isProd: environment.production };
   }
 }
--- apps/frontend/learni/src/app/app.component.ts
+++ apps/frontend/learni/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { environment } from '../environments/environment';
 
 @Component({
   standalone: true,
@@ -10,4 +11,5 @@ import { RouterModule } from '@angular/router';
 })
 export class AppComponent {
   title = 'learni';
+  isProd = environment.production;
 }
